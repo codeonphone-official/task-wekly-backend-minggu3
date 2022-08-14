69441b68ddf330209f8c69b20a2a08cb40e2026f
itsmelief <sativao294@gmail.com>
2022-08-14 20:47:39 +0700

    My Program

--- main.py
+++ main.py
@@ -3,9 +3,19 @@
 #TODO: Tugas 1
 # Buatlah Program Python Menghitung Luas Lingkaran
 #FIXME:
+print('Menghitung Luas Lingkaran')
+r = float(input('Masukan jari-jari lingkaran: ')) # r = jari-jari
+phi = 22/7
+luas = phi * r * r
+print(f'Luas lingkaran : {str(luas)} cm²')
 
-
+print()
 
 #TODO: Tugas 2
 # Buatlah Program Python Mencari Luas Segitiga
 #FIXME:
+print('Mencari Luas Segitiga')
+a = float(input('Masukan panjang alas nya : '))
+t = float(input('Masukan tinggi nya       : '))
+luas = 1/2 * a * t
+print(f'Luas segitiga : {str(luas)} cm²')
